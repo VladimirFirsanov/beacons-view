ad480323e0c611739e7bc53f9b883dc2b7d15aa4
Vladimir Firsanov <v.firsanov@bizmax.io>
2020-08-05 10:59:19 +0300

    removed action button

--- src/app/share/services/notification.service.ts
+++ src/app/share/services/notification.service.ts
@@ -9,15 +9,11 @@ export class NotificationService {
   constructor(private _snackBar: MatSnackBar) { }
 
   public showError(messageCode: string, url?: string): void {
-    let snackBarRef = this._snackBar.open(messageCode, 'action', {
+    this._snackBar.open(messageCode, null, {
       duration: 5000,
       horizontalPosition: 'center',
       verticalPosition: 'top',
       panelClass: 'error-snack-bar'
     });
-
-    // snackBarRef.onAction().subscribe(() => {
-    //   console.log("ACTION")
-    // });
   }
 }
