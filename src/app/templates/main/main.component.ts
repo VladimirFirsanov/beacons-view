fc7d2237678a045c20ef0baf3f3bdcb03d0e2419
Vladimir Firsanov <v.firsanov@bizmax.io>
2020-08-13 14:56:44 +0300

    small commit: added unsubscribe

--- src/app/templates/main/main.component.ts
+++ src/app/templates/main/main.component.ts
@@ -1,32 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 
 import { DataService } from '../../share/services/data.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
 
   public filterCtrl: FormControl;
+  private _unsub: Subject<void>;
 
   constructor(
     public dataService: DataService
   ) {
     this.filterCtrl = new FormControl();
+    this._unsub = new Subject();
   }
 
   public ngOnInit(): void {
     this.dataService.fetchMockData();
     this.filterCtrl.valueChanges
     .pipe(
+      takeUntil(this._unsub),
       debounceTime(700),
       distinctUntilChanged()
     )
     .subscribe(() => this.dataService.filterData(this.filterCtrl.value));
+  }
+
+  public ngOnDestroy(): void {
+    this._unsub.next();
+    this._unsub.complete();
   }
 }
